51d253a4ce2d9f35307402928c3c28fd87ddf443
15953186820 <649349502@qq.com>
2019-12-12 14:44:49 +0800

    fix lanip

--- app/Console/Commands/GatewayWorkerServer.php
+++ app/Console/Commands/GatewayWorkerServer.php
@@ -76,7 +76,7 @@ class GatewayWorkerServer extends Command
         $gateway = new Gateway("websocket://0.0.0.0:7272");
         $gateway->name                 = 'Gateway';                         #设置Gateway进程的名称，方便status命令中查看统计
         $gateway->count                = 1;                                 #进程的数量
-        $gateway->lanIp                = '127.0.0.1';                       #内网ip,多服务器分布式部署的时候需要填写真实的内网ip
+        $gateway->lanIp                = 'competition.jinzhuanglvshi.com';                       #内网ip,多服务器分布式部署的时候需要填写真实的内网ip
         $gateway->startPort            = 7272;                              #监听本机端口的起始端口
         $gateway->pingInterval         = 30;
         $gateway->pingNotResponseLimit = 0;                                 #服务端主动发送心跳
